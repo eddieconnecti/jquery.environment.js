3414795ecefb65c47306f2bfe5e5edc67cf0b2ec
eddieconnecti <eddieconnecti@users.noreply.github.com>
2015-04-28 14:00:12 +0200

    Update jquery.environment.js

--- src/jquery.environment.js
+++ src/jquery.environment.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery Environment
- * Copyright 2015 K. Feldmaier (eddieconnecti@googlemail.com)
+ * Copyright 2015 Kai Feldmaier (kai.feldmaier@gmail.com)
  * Licensed under MIT (http://opensource.org/licenses/MIT)
  */
 
